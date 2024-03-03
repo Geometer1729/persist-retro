6df3e8d1cb12ac57b542c5bed178fb4c0d2e7bb8
Geometer1729 <16kuhnb@gmail.com>
2024-03-03 10:24:50 -0500

    support `environment.persistence.<dir>.users`

--- nixos.nix
+++ nixos.nix
@@ -1,40 +1,76 @@
 { config, lib, ... }:
 let
   scriptFor = import ./gen-script.nix lib;
+  concatMapFlatten = func: attrs:
+    builtins.concatLists (builtins.attrValues (builtins.mapAttrs func attrs));
+  getUserBinds =
+    mount_path: user:
+    (builtins.map
+      (entry:
+      if entry.filePath == entry.home + "/" + entry.file
+      && entry.persistentStoragePath == mount_path
+      then
+        {
+          inherit mount_path;
+          root_path = entry.filePath;
+        }
+      else null
+      )
+      user.files
+    )
+    ++
+    (builtins.map
+      (entry:
+      if entry.dirPath == entry.home + "/" + entry.directory
+      && entry.persistentStoragePath == mount_path
+      then
+        {
+          inherit mount_path;
+          root_path = entry.dirPath;
+        }
+      else null
+      )
+      user.directories
+    );
   binds =
-    builtins.concatLists
-      (builtins.attrValues
-        (builtins.mapAttrs
-          (mount_path: opts:
-            builtins.filter builtins.isAttrs
-            (
+    concatMapFlatten
+      (mount_path: opts:
+        builtins.filter builtins.isAttrs
+          (
             (builtins.map
               (entry:
-                if entry.dirPath == entry.directory && entry.persistentStoragePath == mount_path
+                if entry.dirPath == entry.directory
+                && entry.persistentStoragePath == mount_path
                 then
-                {
-                  inherit mount_path;
-                  root_path = entry.dirPath;
-                }
+                  {
+                    inherit mount_path;
+                    root_path = entry.dirPath;
+                  }
                 else null
               )
               opts.directories)
             ++
             (builtins.map
               (entry:
-                if entry.filePath == entry.file && entry.persistentStoragePath == mount_path
+                if entry.filePath == entry.file
+                && entry.persistentStoragePath == mount_path
                 then
-                {
-                  inherit mount_path;
-                  root_path = entry.filePath;
-                }
+                  {
+                    inherit mount_path;
+                    root_path = entry.filePath;
+                  }
                 else null
               )
               opts.files)
+            ++
+            (concatMapFlatten
+              (_name: user: getUserBinds mount_path user)
+              opts.users
             )
           )
-          config.environment.persistence
-        ));
+      )
+      config.environment.persistence
+  ;
 in
 {
   system.activationScripts.createPersistentStorageDirs.deps = [ "persist-retro" ];
